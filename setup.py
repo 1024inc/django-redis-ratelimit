8ba01b3ebec0453f4afe5c18cd5ef6fa9c28d2c2
Filipe Cabaco <filipecabaco@gmail.com>
2020-03-13 10:22:30 +0000

    Bump up django version requirements

--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ setup(
     ],
     keywords='django redis rate-limit ratelimit',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    install_requires=['Django==1.11.5', 'redis'],
+    install_requires=['Django==1.11.29', 'redis'],
     extras_require={"dev": ["black==19.3b0", "ipdb==0.12.2"]},
 )
